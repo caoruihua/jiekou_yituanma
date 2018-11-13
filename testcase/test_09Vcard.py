84f686b1550c3623aeb6501a98b416f34f94e5eb
Administrator <499800885@qq.com>
2018-11-13 16:52:45 +0800

    整理代码格式

--- testcase/test_09Vcard.py
+++ testcase/test_09Vcard.py
@@ -1,17 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @File  : test_09Vcard.py
-# @Author: Feng
-# @Date  : 2018/11/1
-# @Desc  :
-
-# !/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @File  : test_02approval.py
-# @Author: Feng
-# @Date  : 2018/11/1
-# @Desc  :
-
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 # @File  : test_05GTP.py
@@ -20,29 +6,26 @@
 # @Desc  :
 
 
-import unittest
-import requests
 import json
 import sys
-import os
+import unittest
+import requests
 
 sys.path.append('../..')
-
-from config.config import *
 from lib.db import *
 from lib.read_excel import *
 from lib.case_log import log_case_info
 
 
 class TestVCARD(unittest.TestCase):
-    '''企业名片'''
+    """企业名片"""
 
     @classmethod
     def setUpClass(cls):
         cls.data_list = excel_to_list(os.path.join(data_path, "test_user_data.xlsx"), "VCARD")  # 读取TestUserReg工作簿的所有数据
 
     def test_01VCARD(self):
-        u'''进入企业名片'''
+        u"""进入企业名片"""
         case_data = get_test_data(self.data_list, 'test_VCARD')
         if not case_data:
             logging.error("用例数据不存在")
@@ -56,7 +39,7 @@ class TestVCARD(unittest.TestCase):
         self.assertEqual(res.status_code, expect_res)
 
     def test_02VCARD_authroizer(self):
-        u'''进入企业名片'''
+        u"""进入企业名片"""
         case_data = get_test_data(self.data_list, 'test_VCARD_authroizer')
         if not case_data:
             logging.error("用例数据不存在")
@@ -69,64 +52,62 @@ class TestVCARD(unittest.TestCase):
         # 响应断言（整体断言）
         self.assertEqual(res.status_code, expect_res)
 
-
-    def test_03VCARD_mingpianjia(self):
-        u'''进入企业名片'''
+    def test_03VCARD_Mingpianjia(self):
+        u"""进入企业名片"""
         case_data = get_test_data(self.data_list, 'test_VCARD_mingpianjia')
         if not case_data:
             logging.error("用例数据不存在")
         url = case_data.get('url')
         data = case_data.get('data')  # 转为字典，需要取里面的name进行数据库检查
         expect_res = case_data.get('expect_res')  # 转为字典，断言时直接断言两个字典是否相等
-        res = requests.post(url=url,data=data.encode(),headers=hearder2)  # 用data=data 传字符串也可以
+        res = requests.post(url=url, data=data.encode(), headers=hearder2)  # 用data=data 传字符串也可以
         # 期望响应结果，注意字典格式和json格式的区别（如果有true/false/null要转化为字典格式）
         log_case_info('test_VCARD_mingpianjia', url, data, expect_res, json.dumps(res.json(), ensure_ascii=False))
         # 响应断言（整体断言）
         self.assertEqual(res.status_code, expect_res)
 
     def test_04VCARD_pingjia(self):
-        u'''进入企业名片'''
+        u"""进入企业名片"""
         case_data = get_test_data(self.data_list, 'test_VCARD_pingjia')
         if not case_data:
             logging.error("用例数据不存在")
         url = case_data.get('url')
         data = case_data.get('data')  # 转为字典，需要取里面的name进行数据库检查
         expect_res = case_data.get('expect_res')  # 转为字典，断言时直接断言两个字典是否相等
-        res = requests.get(url=url,headers=hearder2)  # 用data=data 传字符串也可以
+        res = requests.get(url=url, headers=hearder2)  # 用data=data 传字符串也可以
         # 期望响应结果，注意字典格式和json格式的区别（如果有true/false/null要转化为字典格式）
         log_case_info('test_VCARD_pingjia', url, data, expect_res, json.dumps(res.json(), ensure_ascii=False))
         # 响应断言（整体断言）
         self.assertEqual(res.status_code, expect_res)
 
-
     def test_05VCARD_report(self):
-        u'''进入企业名片'''
+        u"""进入企业名片"""
         case_data = get_test_data(self.data_list, 'test_VCARD_report')
         if not case_data:
             logging.error("用例数据不存在")
         url = case_data.get('url')
         data = case_data.get('data')  # 转为字典，需要取里面的name进行数据库检查
         expect_res = case_data.get('expect_res')  # 转为字典，断言时直接断言两个字典是否相等
-        res = requests.get(url=url,headers=hearder2)  # 用data=data 传字符串也可以
+        res = requests.get(url=url, headers=hearder2)  # 用data=data 传字符串也可以
         # 期望响应结果，注意字典格式和json格式的区别（如果有true/false/null要转化为字典格式）
         log_case_info('test_VCARD_report', url, data, expect_res, json.dumps(res.json(), ensure_ascii=False))
         # 响应断言（整体断言）
         self.assertEqual(res.status_code, expect_res)
 
-
     def test_06VCARD_history(self):
-        u'''进入企业名片'''
+        u"""进入企业名片"""
         case_data = get_test_data(self.data_list, 'test_VCARD_history')
         if not case_data:
             logging.error("用例数据不存在")
         url = case_data.get('url')
         data = case_data.get('data')  # 转为字典，需要取里面的name进行数据库检查
         expect_res = case_data.get('expect_res')  # 转为字典，断言时直接断言两个字典是否相等
-        res = requests.get(url=url,headers=hearder2)  # 用data=data 传字符串也可以
+        res = requests.get(url=url, headers=hearder2)  # 用data=data 传字符串也可以
         # 期望响应结果，注意字典格式和json格式的区别（如果有true/false/null要转化为字典格式）
         log_case_info('test_VCARD_history', url, data, expect_res, json.dumps(res.json(), ensure_ascii=False))
         # 响应断言（整体断言）
         self.assertEqual(res.status_code, expect_res)
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)  # 运行所有用例
--- testcase/test_19uaa.py
+++ testcase/test_19uaa.py
@@ -6,73 +6,70 @@
 # @Desc  :
 
 
-
-import unittest
-import requests
 import json
 import sys
-import os
+import unittest
+
+import requests
+
 sys.path.append('../..')
 
-from config.config import *
 from lib.db import *
 from lib.read_excel import *
 from lib.case_log import log_case_info
 
 
-
 class UAA(unittest.TestCase):
-    '''邀请相关'''
+    """邀请相关"""
 
     @classmethod
     def setUpClass(cls):
         cls.data_list = excel_to_list(os.path.join(data_path, "test_user_data.xlsx"), "uaa")  # 读取TestUserReg工作簿的所有数据
 
     def test_01_uaa_invite_mobile(self):
-        u'''手机号码邀请'''
+        u"""手机号码邀请"""
         case_data = get_test_data(self.data_list, 'test_uaa_invite_mobile')
         if not case_data:
             logging.error("用例数据不存在")
         url = case_data.get('url')
         data = case_data.get('data')  # 转为字典，需要取里面的name进行数据库检查
         expect_res = case_data.get('expect_res')  # 转为字典，断言时直接断言两个字典是否相等
-        res = requests.post(url=url,data=data.encode(),headers=hearder2)  # 用data=data 传字符串也可以
+        res = requests.post(url=url, data=data.encode(), headers=hearder2)  # 用data=data 传字符串也可以
         # 期望响应结果，注意字典格式和json格式的区别（如果有true/false/null要转化为字典格式）
         log_case_info('test_uaa_invite_mobile', url, data, expect_res, json.dumps(res.json(), ensure_ascii=False))
         # 响应断言（整体断言）
         self.assertEqual(res.status_code, expect_res)
 
     def test_02_uaa_invite_qrcode(self):
-        u'''二维码邀请成员'''
+        u"""二维码邀请成员"""
         case_data = get_test_data(self.data_list, 'test_uaa_invite_qrcode')
         if not case_data:
             logging.error("用例数据不存在")
         url = case_data.get('url')
         data = case_data.get('data')  # 转为字典，需要取里面的name进行数据库检查
         expect_res = case_data.get('expect_res')  # 转为字典，断言时直接断言两个字典是否相等
-        res = requests.post(url=url,data=data.encode(), headers=hearder2)  # 用data=data 传字符串也可以
+        res = requests.post(url=url, data=data.encode(), headers=hearder2)  # 用data=data 传字符串也可以
         # 期望响应结果，注意字典格式和json格式的区别（如果有true/false/null要转化为字典格式）
         log_case_info('test_uaa_invite_qrcode', url, data, expect_res, json.dumps(res.json(), ensure_ascii=False))
         # 响应断言（整体断言）
         self.assertEqual(res.status_code, expect_res)
 
-
     def test_03_uaa_LoginWrong(self):
-        u'''错误密码登录'''
+        u"""错误密码登录"""
         case_data = get_test_data(self.data_list, 'test_uaa_login_wrong')
         if not case_data:
             logging.error("用例数据不存在")
         url = case_data.get('url')
         data = case_data.get('data')  # 转为字典，需要取里面的name进行数据库检查
         expect_res = case_data.get('expect_res')  # 转为字典，断言时直接断言两个字典是否相等
-        res = requests.post(url=url,data=data.encode(), headers=hearder4)  # 用data=data 传字符串也可以
+        res = requests.post(url=url, data=data.encode(), headers=hearder4)  # 用data=data 传字符串也可以
         # 期望响应结果，注意字典格式和json格式的区别（如果有true/false/null要转化为字典格式）
         log_case_info('test_uaa_login_wrong', url, data, expect_res, json.dumps(res.json(), ensure_ascii=False))
         # 响应断言（整体断言）
         self.assertEqual(res.status_code, expect_res)
 
     def test_04_uaa_Wrong_verifycode(self):
-        u'''错误验证码注册'''
+        u"""错误验证码注册"""
         case_data = get_test_data(self.data_list, 'test_uaa_wrong_verifycode')
         if not case_data:
             logging.error("用例数据不存在")
@@ -84,11 +81,6 @@ class UAA(unittest.TestCase):
         log_case_info('test_uaa_wrong_verify_code', url, data, expect_res, json.dumps(res.json(), ensure_ascii=False))
         # 响应断言（整体断言）
         self.assertEqual(res.status_code, expect_res)
-
-
-
-
-
 
 
 if __name__ == '__main__':

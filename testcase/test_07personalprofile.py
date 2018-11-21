69ed52643e9f3562296570d47b12187f136785b9
apple <499800885@qq.com>
2018-11-21 20:46:25 +0800

    调整格式

--- testcase/test_07personalprofile.py
+++ testcase/test_07personalprofile.py
@@ -1,9 +1,4 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @File  : test_07personalprofile.py
-# @Author: Feng
-# @Date  : 2018/10/31
-# @Desc  :
+
 
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
@@ -29,23 +24,23 @@ from lib.case_log import log_case_info
 
 
 class PersonalProfile(unittest.TestCase):
-    u'''个人相关'''
+    u"""个人相关"""
 
     @classmethod
     def setUpClass(cls):
         cls.data_list = excel_to_list(os.path.join(data_path, "test_user_data.xlsx"),
                                       "personalprofile")  # 读取TestUserReg工作簿的所有数据
-        cls.header7=Login.head()
+        cls.header7 = Login.head()
 
     def test_01_geren_baseinfo(self):
-        u'''基本信息界面'''
+        u"""基本信息界面"""
         case_data = get_test_data(self.data_list, 'test_user_geren_baseinfo')
         if not case_data:
             logging.error("用例数据不存在")
         url = case_data.get('url')
         data = case_data.get('data')  # 转为字典，需要取里面的name进行数据库检查
         expect_res = case_data.get('expect_res')  # 转为字典，断言时直接断言两个字典是否相等
-        header8=self.header7
+        header8 = self.header7
         res = requests.get(url=url, headers=header8)  # 用data=data 传字符串也可以
         # 期望响应结果，注意字典格式和json格式的区别（如果有true/false/null要转化为字典格式）
         log_case_info('test_user_reg_normal', url, data, expect_res, json.dumps(res.json(), ensure_ascii=False))
@@ -53,7 +48,7 @@ class PersonalProfile(unittest.TestCase):
         self.assertEqual(res.status_code, expect_res)
 
     def test_02_geren_weimingpian(self):
-        u'''进入微名片'''
+        u"""进入微名片"""
         case_data = get_test_data(self.data_list, 'test_user_geren_WeimingPian')
         if not case_data:
             logging.error("用例数据不存在")
@@ -68,7 +63,7 @@ class PersonalProfile(unittest.TestCase):
         self.assertEqual(res.status_code, expect_res)
 
     def test_03_geren_ziliao(self):
-        u'''进入微名片'''
+        u"""进入微名片"""
         case_data = get_test_data(self.data_list, 'test_user_geren_ziliao')
         if not case_data:
             logging.error("用例数据不存在")
@@ -83,7 +78,7 @@ class PersonalProfile(unittest.TestCase):
         self.assertEqual(res.status_code, expect_res)
 
     def test_04_geren_weixin(self):
-        u'''进入设置界面'''
+        u"""进入设置界面"""
         case_data = get_test_data(self.data_list, 'test_user_geren_weixin')
         if not case_data:
             logging.error("用例数据不存在")
@@ -98,7 +93,7 @@ class PersonalProfile(unittest.TestCase):
         self.assertEqual(res.status_code, expect_res)
 
     def test_05_geren_weixin(self):
-        u'''个人更多角色'''
+        u"""个人更多角色"""
         case_data = get_test_data(self.data_list, 'test_user_geren_more')
         if not case_data:
             logging.error("用例数据不存在")
@@ -113,7 +108,7 @@ class PersonalProfile(unittest.TestCase):
         self.assertEqual(res.status_code, expect_res)
 
     def test_06_geren_language(self):
-        u'''个人档案-语言能力'''
+        u"""个人档案-语言能力"""
         case_data = get_test_data(self.data_list, 'test_user_geren_language')
         if not case_data:
             logging.error("用例数据不存在")
@@ -128,7 +123,7 @@ class PersonalProfile(unittest.TestCase):
         self.assertEqual(res.status_code, expect_res)
 
     def test_07_geren_avoid_relative(self):
-        u'''个人档案-回避关系'''
+        u"""个人档案-回避关系"""
         case_data = get_test_data(self.data_list, 'test_user_geren_avoid_relative')
         if not case_data:
             logging.error("用例数据不存在")
@@ -144,7 +139,7 @@ class PersonalProfile(unittest.TestCase):
         self.assertEqual(res.status_code, expect_res)
 
     def test_08_geren_experience_list(self):
-        u'''个人档案-工作经历'''
+        u"""个人档案-工作经历"""
         case_data = get_test_data(self.data_list, 'test_user_geren_experience_list')
         if not case_data:
             logging.error("用例数据不存在")
@@ -160,7 +155,7 @@ class PersonalProfile(unittest.TestCase):
         self.assertEqual(res.status_code, expect_res)
 
     def test_09_geren_career(self):
-        u'''个人档案-职业信息'''
+        u"""个人档案-职业信息"""
         case_data = get_test_data(self.data_list, 'test_user_geren_career')
         if not case_data:
             logging.error("用例数据不存在")
@@ -175,7 +170,7 @@ class PersonalProfile(unittest.TestCase):
         self.assertEqual(res.status_code, expect_res)
 
     def test_10_geren_skill_list(self):
-        u'''个人档案-技能'''
+        u"""个人档案-技能"""
         case_data = get_test_data(self.data_list, 'test_user_geren_skill_list')
         if not case_data:
             logging.error("用例数据不存在")
@@ -190,7 +185,7 @@ class PersonalProfile(unittest.TestCase):
         self.assertEqual(res.status_code, expect_res)
 
     def test_11_geren_social_relative(self):
-        u'''个人档案-社会关系'''
+        u"""个人档案-社会关系"""
         case_data = get_test_data(self.data_list, 'test_user_geren_social_relative')
         if not case_data:
             logging.error("用例数据不存在")
@@ -206,7 +201,7 @@ class PersonalProfile(unittest.TestCase):
         self.assertEqual(res.status_code, expect_res)
 
     def test_12_geren_certificate(self):
-        u'''个人档案-职业资格'''
+        u"""个人档案-职业资格"""
         case_data = get_test_data(self.data_list, 'test_user_geren_certificate')
         if not case_data:
             logging.error("用例数据不存在")
@@ -221,7 +216,7 @@ class PersonalProfile(unittest.TestCase):
         self.assertEqual(res.status_code, expect_res)
 
     def test_13_geren_train(self):
-        u'''个人档案-培训经历'''
+        u"""个人档案-培训经历"""
         case_data = get_test_data(self.data_list, 'test_user_geren_train')
         if not case_data:
             logging.error("用例数据不存在")
@@ -236,7 +231,7 @@ class PersonalProfile(unittest.TestCase):
         self.assertEqual(res.status_code, expect_res)
 
     def test_14_geren_contract(self):
-        u'''个人档案-合同'''
+        u"""个人档案-合同"""
         case_data = get_test_data(self.data_list, 'test_user_geren_contract')
         if not case_data:
             logging.error("用例数据不存在")
@@ -251,7 +246,7 @@ class PersonalProfile(unittest.TestCase):
         self.assertEqual(res.status_code, expect_res)
 
     def test_15_geren_base(self):
-        u'''个人档案-基本资料'''
+        u"""个人档案-基本资料"""
         case_data = get_test_data(self.data_list, 'test_user_geren_base')
         if not case_data:
             logging.error("用例数据不存在")
@@ -266,7 +261,7 @@ class PersonalProfile(unittest.TestCase):
         self.assertEqual(res.status_code, expect_res)
 
     def test_16_geren_jiaoyu(self):
-        u'''个人档案-教育经历'''
+        u"""个人档案-教育经历"""
         case_data = get_test_data(self.data_list, 'test_user_geren_jiaoyu')
         if not case_data:
             logging.error("用例数据不存在")
@@ -281,7 +276,7 @@ class PersonalProfile(unittest.TestCase):
         self.assertEqual(res.status_code, expect_res)
 
     def test_17_geren_jinjilianxi(self):
-        u'''个人档案-紧急联系人'''
+        u"""个人档案-紧急联系人"""
         case_data = get_test_data(self.data_list, 'test_user_geren_jinjilianxi')
         if not case_data:
             logging.error("用例数据不存在")
@@ -296,7 +291,7 @@ class PersonalProfile(unittest.TestCase):
         self.assertEqual(res.status_code, expect_res)
 
     def test_18_geren_zhixuxinxi(self):
-        u'''个人档案-职务信息'''
+        u"""个人档案-职务信息"""
         case_data = get_test_data(self.data_list, 'test_user_geren_zhiwuxinxi')
         if not case_data:
             logging.error("用例数据不存在")
@@ -311,7 +306,7 @@ class PersonalProfile(unittest.TestCase):
         self.assertEqual(res.status_code, expect_res)
 
     def test_20_geren_conpetency(self):
-        u'''个人档案-能力'''
+        u"""个人档案-能力"""
         case_data = get_test_data(self.data_list, 'test_user_geren_conpetency')
         if not case_data:
             logging.error("用例数据不存在")
@@ -326,7 +321,7 @@ class PersonalProfile(unittest.TestCase):
         self.assertEqual(res.status_code, expect_res)
 
     def test_21_geren_project(self):
-        u'''个人档案-项目经验'''
+        u"""个人档案-项目经验"""
         case_data = get_test_data(self.data_list, 'test_user_geren_project')
         if not case_data:
             logging.error("用例数据不存在")
@@ -341,7 +336,7 @@ class PersonalProfile(unittest.TestCase):
         self.assertEqual(res.status_code, expect_res)
 
     def test_22_geren_addressbook(self):
-        u'''查看通讯录'''
+        u"""查看通讯录"""
         case_data = get_test_data(self.data_list, 'test_user_geren_addressbook')
         if not case_data:
             logging.error("用例数据不存在")
@@ -356,7 +351,7 @@ class PersonalProfile(unittest.TestCase):
         self.assertEqual(res.status_code, expect_res)
 
     def test_23_geren_addtag(self):
-        u'''添加个人资料标签'''
+        u"""添加个人资料标签"""
         case_data = get_test_data(self.data_list, 'test_user_geren_addtag')
         if not case_data:
             logging.error("用例数据不存在")
@@ -371,7 +366,7 @@ class PersonalProfile(unittest.TestCase):
         self.assertEqual(res.status_code, expect_res)
 
     def test_24_geren_guanzhu(self):
-        u'''关注某人'''
+        u"""关注某人"""
         case_data = get_test_data(self.data_list, 'test_user_geren_guanzhu')
         if not case_data:
             logging.error("用例数据不存在")
@@ -386,7 +381,7 @@ class PersonalProfile(unittest.TestCase):
         self.assertEqual(res.status_code, expect_res)
 
     def test_25_geren_GuanzhuQuxiao(self):
-        u'''取消关注某人'''
+        u"""取消关注某人"""
         case_data = get_test_data(self.data_list, 'test_user_geren_guanzhuquxiao')
         if not case_data:
             logging.error("用例数据不存在")
@@ -402,7 +397,7 @@ class PersonalProfile(unittest.TestCase):
         self.assertEqual(res.status_code, expect_res)
 
     def test_26_geren_join_invite(self):
-        u'''错误邀请码验证'''
+        u"""错误邀请码验证"""
         case_data = get_test_data(self.data_list, 'test_user_geren_join_invite')
         if not case_data:
             logging.error("用例数据不存在")
@@ -417,7 +412,7 @@ class PersonalProfile(unittest.TestCase):
         self.assertEqual(res.status_code, expect_res)
 
     def test_27_geren_join_record(self):
-        u'''查看申请记录'''
+        u"""查看申请记录"""
         case_data = get_test_data(self.data_list, 'test_user_geren_join_record')
         if not case_data:
             logging.error("用例数据不存在")
@@ -432,7 +427,7 @@ class PersonalProfile(unittest.TestCase):
         self.assertEqual(res.status_code, expect_res)
 
     def test_28_geren_kapiandianzan(self):
-        u'''个人资料卡片点赞'''
+        u"""个人资料卡片点赞"""
         case_data = get_test_data(self.data_list, 'test_user_geren_aihao')
         if not case_data:
             logging.error("用例数据不存在")
@@ -462,7 +457,7 @@ class PersonalProfile(unittest.TestCase):
         self.assertEqual(res.status_code, expect_res)
 
     def test_30_user_cache(self):
-        u'''他人查看卡片信息'''
+        u"""他人查看卡片信息"""
         case_data = get_test_data(self.data_list, 'test_user_cache')
         if not case_data:
             logging.error("用例数据不存在")
@@ -477,7 +472,7 @@ class PersonalProfile(unittest.TestCase):
         self.assertEqual(res.status_code, expect_res)
 
     def test_31_user_Jieshao_Save(self):
-        u'''编辑自我介绍'''
+        u"""编辑自我介绍"""
         case_data = get_test_data(self.data_list, 'test_user_jieshao_save')
         if not case_data:
             logging.error("用例数据不存在")
@@ -565,10 +560,6 @@ class PersonalProfile(unittest.TestCase):
         log_case_info('test_use_language_add', url, data, expect_res, json.dumps(res.json(), ensure_ascii=False))
         # 响应断言（整体断言）
         self.assertEqual(res.status_code, expect_res)
-
-
-
-
 
 
 if __name__ == '__main__':

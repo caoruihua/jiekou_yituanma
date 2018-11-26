1c249225a6acbb3f7f9f4cf2c1de114116c0906f
apple <499800885@qq.com>
2018-11-26 18:23:02 +0800

    添加HRM跟ORG脚本

---
+++ testcase/test_hrm.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File  : test_hrm.py
+# @Author: Laocao
+# @Date  : 2018/11/26
+
+
+import json
+import sys
+import unittest
+
+import requests
+
+from lib.util import Login
+
+sys.path.append('../..')
+
+from lib.db import *
+from lib.read_excel import *
+from lib.case_log import log_case_info
+
+
+class HRM(unittest.TestCase):
+    u"""人事报表"""
+
+    @classmethod
+    def setUpClass(cls):
+        cls.data_list = excel_to_list(os.path.join(data_path, "test_user_data.xlsx"), "HRM")  # 读取TestUserReg工作簿的所有数据
+        cls.header7 = Login.head()
+
+    def test_01_Hrm_Number(self):
+        u"""公司人员总数"""
+        case_data = get_test_data(self.data_list, 'test_HRM_renshu')
+        if not case_data:
+            logging.error("用例数据不存在")
+        url = case_data.get('url')
+        data = case_data.get('data')  # 转为字典，需要取里面的name进行数据库检查
+        expect_res = case_data.get('expect_res')  # 转为字典，断言时直接断言两个字典是否相等
+        header8 = self.header7
+        res = requests.get(url=url, headers=header8)  # 用data=data 传字符串也可以
+        # 期望响应结果，注意字典格式和json格式的区别（如果有true/false/null要转化为字典格式）
+        log_case_info('test_HRM_renshu', url, data, expect_res, json.dumps(res.json(), ensure_ascii=False))
+        # 响应断言（整体断言）
+        self.assertEqual(res.status_code, expect_res)
+
+    def test_02_Hrm_jiegou(self):
+        u"""公司人员组成结构"""
+        case_data = get_test_data(self.data_list, 'test_HRM_renyuanjiegou')
+        if not case_data:
+            logging.error("用例数据不存在")
+        url = case_data.get('url')
+        data = case_data.get('data')  # 转为字典，需要取里面的name进行数据库检查
+        expect_res = case_data.get('expect_res')  # 转为字典，断言时直接断言两个字典是否相等
+        header8 = self.header7
+        res = requests.get(url=url, headers=header8)  # 用data=data 传字符串也可以
+        # 期望响应结果，注意字典格式和json格式的区别（如果有true/false/null要转化为字典格式）
+        log_case_info('test_HRM_renyuanjiegou', url, data, expect_res, json.dumps(res.json(), ensure_ascii=False))
+        # 响应断言（整体断言）
+        self.assertEqual(res.status_code, expect_res)
+
+    def test_03_Hrm_In_Number(self):
+        u"""每周入职人员统计"""
+        case_data = get_test_data(self.data_list, 'test_HRM_In_number ')
+        if not case_data:
+            logging.error("用例数据不存在")
+        url = case_data.get('url')
+        data = case_data.get('data')  # 转为字典，需要取里面的name进行数据库检查
+        expect_res = case_data.get('expect_res')  # 转为字典，断言时直接断言两个字典是否相等
+        header8 = self.header7
+        res = requests.get(url=url, headers=header8)  # 用data=data 传字符串也可以
+        # 期望响应结果，注意字典格式和json格式的区别（如果有true/false/null要转化为字典格式）
+        log_case_info('test_HRM_In_number ', url, data, expect_res, json.dumps(res.json(), ensure_ascii=False))
+        # 响应断言（整体断言）
+        self.assertEqual(res.status_code, expect_res)
+
+    def test_04_Hrm_Out_Number(self):
+        u"""每周离职人员统计"""
+        case_data = get_test_data(self.data_list, 'test_HRM_out_number')
+        if not case_data:
+            logging.error("用例数据不存在")
+        url = case_data.get('url')
+        data = case_data.get('data')  # 转为字典，需要取里面的name进行数据库检查
+        expect_res = case_data.get('expect_res')  # 转为字典，断言时直接断言两个字典是否相等
+        header8 = self.header7
+        res = requests.get(url=url, headers=header8)  # 用data=data 传字符串也可以
+        # 期望响应结果，注意字典格式和json格式的区别（如果有true/false/null要转化为字典格式）
+        log_case_info('test_HRM_out_number', url, data, expect_res, json.dumps(res.json(), ensure_ascii=False))
+        # 响应断言（整体断言）
+        self.assertEqual(res.status_code, expect_res)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)  # 运行所有用例
